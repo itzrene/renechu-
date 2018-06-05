b3dd3765047903797f4769c483eac800e956735a
itzrene <renko.krcmar12@gmail.com>
2018-06-05 15:33:10 +0200

    Delete Ref.java

--- Ref.java
+++
@@ -1,9 +0,0 @@
-package me.rene.bots.renechu;
-
-public class Ref {
-
-	public static final String TOKEN = "NDUzNTI4NjUwODEwMjYxNTA0.DfgUlA.wHNfQ_UKhRtOxhE0GtI4y7_Y5Nk";
-	public static final String CLIENT_ID = "453528650810261504";
-	public static final String PREFIX = "!";
-	
-}
